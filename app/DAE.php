b7589fd73a08259af22453b730fc01ac1791945f
evertonmessias <everton.messias@gmail.com>
2023-01-17 10:45:31 -0300

    17-01-2023_10:45:31

--- app/DAE.php
+++ app/DAE.php
@@ -30,35 +30,43 @@ class DAE
 {
     public static function connect($sql)
     {
+        if ($_SESSION['dae'] == "CEBI") {
 
-        $connection = ssh2_connect(ssh_host, ssh_port);
+            $connection = ssh2_connect(ssh_host, ssh_port);
 
-        if (ssh2_auth_password($connection, ssh_user, ssh_pass)) {
-            $conttStream = ssh2_exec($connection, 'source .bash_profile; echo "' . $sql . '" | sqlplus -M "HTML ON" ' . orcl);
+            if (ssh2_auth_password($connection, ssh_user, ssh_pass)) {
+                $conttStream = ssh2_exec($connection, 'source .bash_profile; echo "' . $sql . '" | sqlplus -M "HTML ON" ' . orcl);
 
-            $errorStream = ssh2_fetch_stream($conttStream, SSH2_STREAM_STDERR);
+                $errorStream = ssh2_fetch_stream($conttStream, SSH2_STREAM_STDERR);
 
-            stream_set_blocking($errorStream, true);
-            stream_set_blocking($conttStream, true);
+                stream_set_blocking($errorStream, true);
+                stream_set_blocking($conttStream, true);
 
-            $error = stream_get_contents($errorStream);
-            if ($error != "") {
-                return "Error: " . $error;
+                $error = stream_get_contents($errorStream);
+                if ($error != "") {
+                    return "Error: " . $error;
+                }
+
+                $contt = stream_get_contents($conttStream);
+                if ($contt != "") {
+                    preg_match_all('/SQL&gt;(.*)SQL&gt;/s', $contt, $content);
+                    return $content[1][0];
+                }
+
+                fclose($errorStream);
+                fclose($conttStream);
+            } else {
+                die("<p>Authentication Failed!</p>");
             }
-
-            $contt = stream_get_contents($conttStream);
-            if ($contt != "") {
-                preg_match_all('/SQL&gt;(.*)SQL&gt;/s', $contt, $content);
-                return $content[1][0];
+        } elseif ($_SESSION['dae'] == "ASSESSOR") {
+            try {
+                $conn = new PDO(fdsn, user, pass);
+                return $conn->query($sql);
+            } catch (PDOException $e) {
+                return "ERROR: " . $e->getMessage();
             }
-
-            fclose($errorStream);
-            fclose($conttStream);
-        } else {
-            die("<p>Authentication Failed!</p>");
         }
     }
-
 
     public static function header()
     {
@@ -120,28 +128,38 @@ class DAE
         } else {
             echo self::header();
         ?>
-            <form class="login" method="POST">
+            <form method="POST">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-12">
-                            <br>
+                        <div class="login col-lg-4">
+
                             <div class="form-group">
-                                <label>
-                                    <input type="text" name="user" class="form-control" placeholder="Username" required>
+                                <label>Username
+                                    <input type="text" name="user" class="form-control" placeholder="Enter your Username" required>
                                 </label>
                             </div>
                             <br>
                             <div class="form-group">
-                                <label>
-                                    <input type="password" name="pass" class="form-control" placeholder="Password" required>
+                                <label>Password
+                                    <input type="password" name="pass" class="form-control" placeholder="Enter your Password" required>
                                 </label>
                             </div>
+                            <br>
+
+                            <div class="form-check">
+                                <label class="form-check-label"><img src="/assets/logo_oracle.png" class="img-fluid"><input class="form-check-input" type="radio" name="database" value="CEBI" checked> CEBI</label>
+                            </div>
+                            <div class="form-check">
+                                <label class="form-check-label"><img src="/assets/logo_firebase.png" class="img-fluid"><input class="form-check-input" type="radio" name="database" value="ASSESSOR"> ASSESSOR</label>
+                            </div>
+
                             <br>
                             <button type="submit" name="btnlogin" class="btn btn-primary">Submit</button>
+
                             <?php
                             if (isset($_POST['btnlogin']) &&  isset($_POST['user']) && isset($_POST['pass'])) {
                                 if ($_POST['user'] == USERNAME && $_POST['pass'] == PASSWORD) {
-                                    $_SESSION['dae'] = "CEBI";
+                                    $_SESSION['dae'] = $_POST['database'];
                                     header('Location:select');
                                 } else {
                                     echo "<p class='error-login'>Username or Password <b>Invalid</b> !</p>";
@@ -171,9 +189,12 @@ class DAE
                     <div class="row">
                         <div class="select col-lg-12">
                             <h5>Write a Query:</h5>
-                            <small>
-                            e.g.: SELECT DATA,NOME_DETALHE,VALOR,TIPO,EXERCICIO FROM MOVIMENTO_EMPENHOS_RECEITAS WHERE EXERCICIO LIKE '2022' AND TIPO LIKE 'RECEITA' AND DATA BETWEEN TO_DATE('01-JAN-22','DD-MON-YY') AND TO_DATE('31-DEC-22','DD-MON-YY') ORDER BY 1 DESC;
-                            </small>
+                            <?php
+                            if ($_SESSION['dae'] == 'CEBI') {  ?>
+                                <small>
+                                    e.g.: SELECT DATA,NOME_DETALHE,VALOR,TIPO,EXERCICIO FROM MOVIMENTO_EMPENHOS_RECEITAS WHERE EXERCICIO LIKE '2022' AND TIPO LIKE 'RECEITA' AND DATA BETWEEN TO_DATE('01-JAN-22','DD-MON-YY') AND TO_DATE('31-DEC-22','DD-MON-YY') ORDER BY 1 DESC;
+                                </small>
+                            <?php } ?>
                             <textarea class="form-control" name="query" rows="5"></textarea>
                         </div>
                         <br>
@@ -195,8 +216,7 @@ class DAE
     public static function query()
     {
         echo self::header();
-
-        ?>
+    ?>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet">
         <link type="text/css" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
         <link type="text/css" href="https://cdn.datatables.net/autofill/2.4.0/css/autoFill.bootstrap4.css" rel="stylesheet" />
@@ -205,28 +225,63 @@ class DAE
         <button type="button" class="btn btn-primary btn-floating btn-lg" id="btn-back-to-top"><i class="ri-arrow-up-fill"></i></button>
         <?php
 
-        if (isset($_SESSION['dae']) && isset($_POST['submit']) && $_POST['query']) {
+        if ($_SESSION['dae'] == "CEBI") {
 
-            $sql = $_POST['query'];
+            if (isset($_POST['submit']) && $_POST['query']) {
 
-            preg_match_all('/<tr>(.*?)<\/tr>/s', utf8_encode(self::connect($sql)), $content);
-            $results_table = $content[0];
-            $thead = array_shift($results_table);
-            $tbody = "";
-            foreach ($results_table as $rt) {
-                if ($rt != $thead) {
-                    $tbody .= $rt;
+                $sql = $_POST['query'];
+
+                preg_match_all('/<tr>(.*?)<\/tr>/s', utf8_encode(self::connect($sql)), $content);
+                $results_table = $content[0];
+                $thead = array_shift($results_table);
+                $tbody = "";
+                foreach ($results_table as $rt) {
+                    if ($rt != $thead) {
+                        $tbody .= $rt;
+                    }
                 }
+                $strings_table = "<div class='container'><div class='row'><div class='col-lg-12'><br><table id='result' class='table table-striped table-bordered' style='width:100%'><thead>" . $thead . "</thead><tbody>" . $tbody . "</tbody></table></div></div></div><br>";
+
+                echo $strings_table;
+            } else {
+                echo self::error();
             }
-            $strings_table = "<div class='container'><div class='row'><div class='col-lg-12'><br><table id='result' class='table table-striped table-bordered' style='width:100%'><thead>" . $thead . "</thead><tbody>" . $tbody . "</tbody></table></div></div></div><br>";
 
-            echo $strings_table;
+        } elseif ($_SESSION['dae'] == "ASSESSOR") {
 
+            if (isset($_POST['submit']) && $_POST['query']) {
+
+                $sql = $_POST['query'];
+
+                $array = self::connect($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+                $thead = "<tr>";
+                foreach ($array[0] as $key => $value) {
+                    $thead .= "<th>" . $key . "</th>";
+                }
+                $thead .= "</tr>";
+
+                $tbody = "";
+                foreach ($array as $row) {
+                    $tbody .= "<tr>";
+                    foreach ($row as $key => $value) {
+                        $tbody .= "<td>" . $value . "</td>";
+                    }
+                    $tbody .= "</th>";
+                }
+
+                $strings_table = "<div class='container'><div class='row'><div class='col-lg-12'><br><table id='result' class='table table-striped table-bordered' style='width:100%'><thead>" . $thead . "</thead><tbody>" . utf8_encode($tbody) . "</tbody></table></div></div></div><br>";
+
+                echo $strings_table;
+
+            } else {
+                echo self::error();
+            }
         } else {
             echo self::error();
         }
         echo self::footer();
-    ?>
+        ?>
         <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
@@ -240,17 +295,17 @@ class DAE
         <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.bootstrap4.min.js"></script>
         <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
         <script src="assets/script.js"></script>
-        <?php
+    <?php
     }
 
     public static function error()
     {
-        ?>
+    ?>
         <br>
         <h2>Error 403 Forbidden<br><br>
             <a href='/' title='Go Back'><button type='button' class='btn btn-primary'>&emsp;<i class='ri-skip-back-fill'></i>&emsp;</button></a>
         </h2>
-<?php
+    <?php
     }
 
     public static function logout()
@@ -260,4 +315,32 @@ class DAE
         header('Location:/');
     }
 
+    public static function cebi()
+    {
+    ?>
+        <style>
+            table {
+                border-spacing: 0;
+            }
+            td,
+            th {
+                border: 1px solid #000;
+            }
+        </style>
+    <?php
+
+        $sql = "SELECT DATA,NOME_DETALHE,VALOR,TIPO,EXERCICIO FROM MOVIMENTO_EMPENHOS_RECEITAS WHERE EXERCICIO LIKE '2022' AND TIPO LIKE 'RECEITA' AND DATA BETWEEN TO_DATE('01-JAN-22','DD-MON-YY') AND TO_DATE('31-DEC-22','DD-MON-YY') ORDER BY 1 DESC;";
+
+        preg_match_all('/<tr>(.*?)<\/tr>/s', utf8_encode(DAE::connect($sql)), $content);
+        $results_table = $content[0];
+        $thead = array_shift($results_table);
+        $tbody = "";
+        foreach ($results_table as $rt) {
+            if ($rt != $thead) {
+                $tbody .= $rt;
+            }
+        }
+        $strings_table = "<table><thead>" . $thead . "</thead><tbody>" . $tbody . "</tbody></table>";
+        echo $strings_table;
+    }
 }
